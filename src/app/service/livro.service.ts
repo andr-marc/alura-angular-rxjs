d74406fd5fa8ec075bad95f92e39cd9805cede6d
André Marc <andreluis0015@gmail.com>
2023-08-26 18:20:47 -0300

    + tratamento de erros + contagem de resultados

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -11,12 +11,13 @@ export class LivroService {
 
   constructor(private http: HttpClient) {}
 
-  buscar(valorDigitado: string): Observable<Item[]> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+    return this.http.get<LivrosResultado>(this.API, { params });
+    // .pipe(
       // tap((retornoAPI: any) => console.log(retornoAPI)),
-      map((resultado: any) => resultado.items),
+      // map((resultado: any) => resultado.items ?? []),
       // tap((resultado: any) => console.log(resultado))
-    );
+    // );
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, map, switchMap } from 'rxjs';
-import { Item } from 'src/app/models/interfaces';
+import { EMPTY, catchError, debounceTime, filter, map, of, switchMap, throwError } from 'rxjs';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -13,14 +13,39 @@ const PAUSA = 300
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) {}
+
+  // totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+  //   debounceTime(PAUSA),
+  //   filter((valorDigitado) => valorDigitado.length >= 3),
+  //   switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+  //   map((resultado) => (this.livrosResultado = resultado)),
+  //   catchError(() => {
+  //     return of();
+  //   })
+  // );
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     debounceTime(PAUSA),
     filter((valorDigitado) => valorDigitado.length >= 3),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map((items) => this.livrosResultadoParaLivros(items))
+    map((resultado) => (this.livrosResultado = resultado)),
+    map((resultado) => resultado.items ?? []),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError(() => {
+      // this.mensagemErro = 'Ocorreu um erro. Recarregue a aplicação';
+      // return EMPTY;
+      // console.log(erro);
+      return throwError(
+        (erro) =>
+          new Error(
+            (this.mensagemErro = 'Ocorreu um erro. Recarregue a aplicação')
+          )
+      );
+    })
   );
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
