9a7c5f32bf8ac222970deac2637d29a6c7225a55
André Marc <andreluis0015@gmail.com>
2023-08-26 14:48:37 -0300

    + consumindo API + exibindo dados na listagem

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,17 +1,22 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes';
-  
+
   constructor(private http: HttpClient) {}
 
-  buscar(valorDigitado: string): Observable<any> {
+  buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado);
-    return this.http.get(this.API, {params})
+    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+      tap((retornoAPI: any) => console.log(retornoAPI)),
+      map((resultado: any) => resultado.items),
+      tap((resultado: any) => console.log(resultado))
+    );
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -7,13 +9,40 @@ import { LivroService } from 'src/app/service/livro.service';
   styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent {
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = '';
+  subscription: Subscription;
+  livro: Livro;
 
   constructor(private service: LivroService) {}
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe((retornoAPI) => console.log(retornoAPI));
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (items) => {
+        this.listaLivros = this.livrosResultadoParaLivros(items);
+      },
+      error: (erro) => console.log(erro),
+    });
+  }
+
+  livrosResultadoParaLivros(items): Livro[] {
+    const livros: Livro[] = [];
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail,
+      });
+    });
+    return livros;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
 
